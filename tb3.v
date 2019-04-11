642bb214f0a8d1a5e6e345a5ef1b602932dead05
mahdifarhang <44311707+mahdifarhang@users.noreply.github.com>
2019-04-11 18:48:14 +0430

    adding expanations

--- tb3.v
+++ tb3.v
@@ -1,19 +1,15 @@
 `timescale 1 ns/ 1 ns
 module test_bench();
-
   parameter N = 4;
-
+//to change N from 4 to 8:
+//change the final delays in test bench from 300 to 500. change the a,b numbers in test bench 'pay attention to number of digits 8'b...'
+//change all the Ns in test bench file and modules.
   reg [N-1:0] a, b;
   reg reset, load, clk;
   wire [N-1:0] sum;
   wire cout;
 
   serial_adder Adder (a, b, reset, load, clk, sum, cout);
-
-
-
-
-
 
   initial begin
     clk = 0;
@@ -26,14 +22,13 @@ module test_bench();
     reset = 1'b1;
     #20
     reset = 1'b0;
-    a = 4'b1111;
+    a = 4'b1001;
     b = 4'b0111;
     #20
     load = 1'b1;
     #40
     load = 1'b0;
     #300
-
 
     reset = 1'b1;
     #20
